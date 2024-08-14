2615804e502890a0bc6a84302e149a68947a679c
yuta-hamasaki <yh02052002@gmail.com>
2024-08-13 20:03:01 -0700

    completed

--- app.js
+++ app.js
@@ -1,1 +1,75 @@
-// PUT YOUR CODE HERE
+const vw = window.innerWidth;
+const vh = window.innerHeight;
+const timeline = gsap.timeline();
+
+timeline.from('.red', {
+    x: -200,
+    y: -200,
+  })
+  .from('.green', {
+    x: 200,
+    y: -200,
+  }) 
+  .from('.blue', {
+    x: 200,
+    y: 200,
+  })
+  .from('.yellow', {
+    x: -200,
+    y: 200,
+  });
+  
+timeline.to('.red', {
+      x: vw - 200,
+      y: 0, 
+      borderRadius: "50%",
+      backgroundColor: "green",
+      duration: 1,
+  })
+  .to('.green', {
+      x: 0, 
+      y: vh - 200, 
+      borderRadius: "50%",
+      backgroundColor: "blue",
+      duration: 1,
+  }, "<")
+  .to('.blue', {
+      x: -vw + 200, 
+      y: 0,
+      borderRadius: "50%",
+      backgroundColor: "yellow",
+      duration: 1,
+  }, "<")
+  .to('.yellow', {
+      x: 0,
+      y: -vh + 200,
+      borderRadius: "50%",
+      backgroundColor: "red",
+      duration: 1,
+  }, "<");
+
+    
+timeline.to('.red', {
+      x: +2000,
+      borderRadius: "50%",
+      backgroundColor: "green",
+      duration: 2,
+  },)
+  .to('.green', {
+      x: +2000,
+      borderRadius: "50%",
+      backgroundColor: "blue",
+      duration: 2,
+  }, "<")
+  .to('.blue', {
+      x: -2000, 
+      borderRadius: "50%",
+      backgroundColor: "yellow",
+      duration: 2,
+  }, "<")
+  .to('.yellow', {
+      x: -2000,
+      borderRadius: "50%",
+      backgroundColor: "red",
+      duration: 2,
+  }, "<");
